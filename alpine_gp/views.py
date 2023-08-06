4e713b0636634054824d6d3e984f2895f92f17e6
Vikram Karra <vikramsimhareddykarra23@gmail.com>
2023-08-06 22:00:36 +1200

    Made changes to College, Course, Agents POST Methods

--- alpine_gp/views.py
+++ alpine_gp/views.py
@@ -23,22 +23,47 @@ def getColleges(request):
     if request.method == "POST":
         # Check if college_id is provided in the request data
         college_id = request.data.get("college_id")
+        college_code = request.data.get("college_code")
         if college_id:
             try:
-                # get the existing college record by college_id
-                college = College.objects.get(college_id=college_id)
-                serializer = CollegeSerializer(college, data=request.data)
-            except College.DoesNotExist:
-                # If college_id does not correspond to an existing record, create a new one
-                serializer = CollegeSerializer(data=request.data)
-        else:
-            # If college_id is not provided, create a new record
-            serializer = CollegeSerializer(data=request.data)
+                existing_college = College.objects.get(college_id=college_id)
+                if existing_college.college_code != college_code:
+                    try:
+                        existing_college_with_code = College.objects.get(college_code=college_code)
+                        return Response(
+                            {"error": "College code already exists."},
+                            status=status.HTTP_400_BAD_REQUEST,
+                        )
+                    except College.DoesNotExist:
+                        pass
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                serializer = CollegeSerializer(existing_college, data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            except College.DoesNotExist:
+                return Response(
+                    {"error": "College with the given ID does not exist."},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+
+        else:
+            # If college_id is not provided, check if a college with the given college_code already exists
+            try:
+                existing_college_with_code = College.objects.get(college_code=college_code)
+                return Response(
+                    {"error": "College code already exists."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            except College.DoesNotExist:
+
+                serializer = CollegeSerializer(data=request.data)
+                if serializer.is_valid():
+                    serializer.save()
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 #  get number of college
@@ -55,28 +80,58 @@ def getCourses(request):
         serializer = CourseSerializer(courses, many=True)
         return Response(serializer.data)
     if request.method == "POST":
-        try:
-            data = request.data
-            try:
-                course_id = data.get("crsid")
-                course = Course.objects.get(crsid=course_id)
-                # Update the existing course object
-                course.college_id = data["college_id"]
-                course.course = data["course"]
-                course.duration = data["duration"]
-                course.save()
-            except Course.DoesNotExist:
-                # If no existing Course Object, create a new one
-                course = Course.objects.create(
-                    college_id=data["college_id"],
-                    course=data["course"],
-                    duration=data["duration"],
-                    crsid=data["crsid"],
-                )
-            return Response(data, status=status.HTTP_201_CREATED)
-        except:
-            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        data = request.data
 
+        course_id = data.get("crsid")
+        course_name = data.get("course")
+        try:
+            # get the course with the provided crsid
+            course_instance = Course.objects.get(crsid=course_id)
+
+            # Check if the updated course name exists for the given college
+            if Course.objects.filter(college_id=data["college_id"], course=course_name).exclude(crsid=course_id).exists():
+                return Response(
+                    {"error": "Course name already exists."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # Update the existing course object
+            course_instance.college_id = data["college_id"]
+            course_instance.course = data["course"]
+            course_instance.duration = data["duration"]
+            course_instance.save()
+
+            # Serialize the updated course instance
+            serializer = CourseSerializer(course_instance)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except Course.DoesNotExist:
+            # Check if the provided crsid is unique
+            if Course.objects.filter(crsid=course_id).exists():
+                return Response(
+                    {"error": "crsid already exists"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # Check if the given course name exists for the college
+            if Course.objects.filter(college_id=data["college_id"], course=data["course"]).exists():
+                return Response(
+                    {"error": "Course name already exists."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            # Create the new course object
+            course = Course.objects.create(
+                college_id=data["college_id"],
+                course=data["course"],
+                duration=data["duration"],
+                crsid=data["crsid"],
+            )
+
+            serializer = CourseSerializer(course)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(data, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 # get course by college_id
 @api_view(["GET"])
@@ -138,29 +193,66 @@ def getAgents(request):
         serializer = AgentSerializer(agents, many=True)
         return Response(serializer.data)
 
+    """
+    View to handle creation or update of an Agent.
+    - The 'agentsid' is used to determine if an agent exists.
+    - If 'agentsid' exists and 'update_flag' is true in the request data, the agent is updated.
+    - If 'agentsid' exists and 'update_flag' is absent or false, an error is returned.
+    - If 'agentsid' doesn't exist, a new agent is created.
+    """
     if request.method == "POST":
-        try:
-            data = request.data
-            try:
-                agentsid = data.get("agentsid")
-                agent = Agent.objects.get(agentsid=agentsid)
-                # Update the existing agent object
-                agent.agentname = data.get("agentname", agent.agentname)
-                agent.email = data.get("email", agent.email)
-                agent.contact = data.get("contact", agent.contact)
-                agent.save()
-            except Agent.DoesNotExist:
-                # If no existing Agent object, create a new one
-                agent = Agent.objects.create(
-                    agentsid=data["agentsid"],
-                    agentname=data.get("agentname"),
-                    email=data.get("email"),
-                    contact=data.get("contact"),
-                )
+        data = request.data
+
+        agentsid = data.get("agentsid")
+        if not agentsid:
+            return Response(
+                {"error": "Agent ID is required."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        agent_exists = Agent.objects.filter(agentsid=agentsid).exists()
+        update_flag = data.get('update_flag', False)
+
+        # If the agent with given agentsid exists and update_flag is true
+        if agent_exists and update_flag:
+            agent = Agent.objects.get(agentsid=agentsid)
+
+            # Update the existing agent object
+            agent.agentname = data.get("agentname", agent.agentname)
+            agent.email = data.get("email", agent.email)
+            agent.contact = data.get("contact", agent.contact)
+            agent.save()
+
+            serializer = AgentSerializer(agent)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        # If the agent with given agentsid doesn't exist but update_flag is true
+        elif not agent_exists and update_flag:
+            return Response(
+                {"error": "No Agent found with the provided agentsid for updating."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        # If agent doesn't exist and the intention is to create a new one
+        elif not agent_exists:
+            agent = Agent.objects.create(
+                agentsid=data["agentsid"],
+                agentname=data.get("agentname"),
+                email=data.get("email"),
+                contact=data.get("contact"),
+            )
+
             serializer = AgentSerializer(agent)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        except:
-            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+        # If agent exists but the intention was not to update
+        else:
+            return Response(
+                {"error": "Agent ID already exists."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+    else:
+        return Response({"error": "Invalid request method"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
 # get college by id
